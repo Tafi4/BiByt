c5a06f47f102d66557997dcd8062b5989ec4c0b3
Hypick <twerka420@gmail.com>
2024-04-29 14:57:36 +0300

    add test database

---
+++ BiByt/Database/ApplicationContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace BiByt.Database
+{
+    public class ApplicationContext : DbContext
+    {
+        protected readonly IConfiguration IConfig;
+
+        public ApplicationContext(IConfiguration configuration)
+        {
+            IConfig = configuration;
+            // Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // var host = ConfigurationManager.AppSettings["PG_HOST"];
+            // var port = ConfigurationManager.AppSettings["PG_PORT"];
+            // var username = ConfigurationManager.AppSettings["PG_USERNAME"];
+            // var password = ConfigurationManager.AppSettings["PG_PASSWORD"];
+            // var path = ConfigurationManager.AppSettings["PG_PATH"];
+            if (!optionsBuilder.IsConfigured)
+                // optionsBuilder.UseNpgsql($"Host={host}; Port={port}; Username={username}; Password={password}; Database={path}");
+                optionsBuilder.UseNpgsql(IConfig.GetConnectionString("Database"));
+        }
+
+        // protected override void OnModelCreating(ModelBuilder modelBuilder)
+        // {
+        //     modelBuilder.Entity<User>(entity =>
+        //     {
+        //         // entity.ToTable( "quote_data", "crypto_data");
+        //         entity.Property(b => b.Id)
+        //             .HasColumnName("id")
+        //             .IsRequired();
+        //         entity.Property(b => b.Username)
+        //             .HasColumnName("username");
+        //     });
+        // }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ BiByt/Database/Models.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BiByt.Database
+{
+    // public class Models { }
+
+    public class User
+    {
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public List<int>? TransactionIds { get; set; }
+    }
+
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public int Description { get; set; }
+    }
+}
